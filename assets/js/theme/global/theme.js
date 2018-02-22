b739c66930b055a3a3101610d5b7e988d0a574ce
Keyur Gajjar <keyur.gajjar@rapidops.com>
2018-02-22 11:15:56 +0530

    Solved JS issues and Tasks.

--- assets/js/theme/global/theme.js
+++ assets/js/theme/global/theme.js
@@ -14,42 +14,45 @@ export default function() {
       animated: "medium"
     });
 
+    jQuery(".mobile-search-icon span").click(function () {
+        jQuery(".mobile-search-icon").toggleClass("active");
+        jQuery("#quickSearch").slideToggle(300, function () {
+        });
+    });
+
     //Don't enable Cloud Zoom (product image zoom) on touch device
-//Mouseenter/Mouseover events are not ideal for touch devices
-//for more info search for this code in /script/main.js
-if (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0)){
-  jQuery('#menu ul a.has-subMenu').click(function(e){
-   e.preventDefault();
-   if(!$(this).hasClass("activeli")){
-   $('.activeli').next().fadeOut('fast');
-   $('.activeli').removeClass("activeli");
-   $(this).next().fadeIn('slow');
+    //Mouseenter/Mouseover events are not ideal for touch devices
+    //for more info search for this code in /script/main.js
+    if (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0)){
+      jQuery('#menu ul a.has-subMenu').click(function(e){
+       e.preventDefault();
+       if(!$(this).hasClass("activeli")){
+       $('.activeli').next().fadeOut('fast');
+       $('.activeli').removeClass("activeli");
+       $(this).next().fadeIn('slow');
 
-   $(this).addClass("activeli")
-   } else if($(this).hasClass("activeli")){
-   $(this).removeClass("activeli");
-   $(this).next().fadeOut('fast');
-   location.href = $(this).attr("href");
-   }
-   });
-}
+       $(this).addClass("activeli")
+       } else if($(this).hasClass("activeli")){
+       $(this).removeClass("activeli");
+       $(this).next().fadeOut('fast');
+       location.href = $(this).attr("href");
+       }
+       });
+    }
 
-//search results close on click on body
-$('html,body').click(function(e){
+    //search results close on click on body
+    $('html,body').click(function(e){
 
-    if($('.quickSearchResults').hasClass('visible') && $(e.target).attr('id')!="search_query" && $(e.target).parents('.quickSearchResults').length==0){
-      $('.quickSearchResults').removeClass('visible');
-    $('.quickSearchResults').hide();
-    $('.quickSearchResults').html("");
-  }
-});
+        if($('.quickSearchResults').hasClass('visible') && $(e.target).attr('id')!="search_query" && $(e.target).parents('.quickSearchResults').length==0){
+          $('.quickSearchResults').removeClass('visible');
+        $('.quickSearchResults').hide();
+        $('.quickSearchResults').html("");
+      }
+    });
 
-$('.quickSearchResults').parent().find('input').change(function(){
-  $('.quickSearchResults').addClass('visible').show();
-});
-
-
-
+    $('.quickSearchResults').parent().find('input').change(function(){
+      $('.quickSearchResults').addClass('visible').show();
+    });
 
     /*  Fancybox  */
     $('.fancythumb').click(function(){
